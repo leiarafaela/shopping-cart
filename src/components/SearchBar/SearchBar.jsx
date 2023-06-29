789903fc9a0604ff4b24e5bf9ae084685ff2081c
rafaela <rafaela20augusto@gmail.com>
2023-06-29 09:08:55 -0300

    search bar styling

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { BsSearch } from 'react-icons/bs';
 
 import './SearchBar.css';
 
+
 function SearchBar(){
+
+  const [searchValue, setSearchValue] = useState('exemplo');
+
   return (
     <form className="search-bar">
       <input type="search"
+        value={searchValue}
         placeholder="Buscar produtos"
         className="search__input"
+        onChange={({target}) => setSearchValue(target.value)}
         required
       />
 
-      <button type="submit" className="search__input">
+      <button type="submit" className="search__button">
         <BsSearch/>
       </button>
     </form>
